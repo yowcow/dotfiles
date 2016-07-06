e31536aed2463d56816b6d27d9b1f970d5596782
yowcow <yowcow@cpan.org>
2016-07-06 07:15:31 +0000

    add Mojo tools

--- cpanm-install.sh
+++ cpanm-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cpanm -n \
-    EV \
     AnyEvent \
     App::cpanoutdated \
     Carton \
     common::sense \
+    EV \
     ExtUtils::MakeMaker \
     JSON::XS \
     Log::Dispatch::CronoDir \
@@ -13,17 +13,26 @@ cpanm -n \
     Log::Log4perl \
     LWP::UserAgent \
     LWP::Protocol::https \
+    Minilla \
     Mojolicious \
+    MojoX::Log::Log4perl::Tiny \
+    MojoX::Renderer::JSON::XS \
+    MojoX::Session::Simple \
     Parallel::Prefork \
     Perl::Tidy \
     Plack \
     Starlet \
     String::CamelCase \
     String::Random \
+    Test::CPAN::Meta \
     Test::Deep \
     Test::Exception \
+    Test::MinimumVersion::Fast \
     Test::Mock::Guard \
     Test::More \
+    Test::PAUSE::Permissions \
+    Test::Pod \
+    Test::Spellunker \
     Test::Stub \
     Time::Piece \
     TOML::Parser \
