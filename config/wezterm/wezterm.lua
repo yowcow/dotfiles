faff1e68d8cd26fce7d43a251fe787204bf5bc35
Yoko OYAMA <yowcow@gmail.com>
2022-03-19 17:45:34 +0900

    tweak

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,9 @@
 local wezterm = require 'wezterm';
-local font_size = 5.0;
+local font_size = 11.0;
+
+if package.config:sub(1,1) == "/" then
+  -- Unix-like
+end
 
 return {
   color_scheme = "Molokai",
@@ -8,7 +12,10 @@ return {
     harfbuzz_features = {"calt=0", "clig=0", "liga=0"}, -- disable ligature feature
   }),
   font_size = font_size,
+  initial_cols = 160,
+  initial_rows = 48,
   tab_bar_at_bottom = true,
+  use_ime = true,
   window_frame = {
     font_size = font_size,
   },
