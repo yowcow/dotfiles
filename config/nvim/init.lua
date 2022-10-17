6fe233e2125c96729e1d3d8e23fd8599f049b3dc
Yoko OYAMA <yowcow@gmail.com>
2022-10-17 10:08:51 +0900

    fix cmp_nvim_lsp capabilities

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -216,7 +216,7 @@ end
 -- :help lsp
 --
 -- vim.lsp.set_log_level('debug')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require 'lspconfig'
 local lspservers = {
   'ansiblels',
