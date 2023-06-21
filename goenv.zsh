d4ecb567547d68b74aeed78f489f55f5ac60ffcc
Yoko OYAMA <yowcow@gmail.com>
2023-06-21 13:51:23 +0900

    fix??

--- goenv.zsh
+++ goenv.zsh
@@ -1,13 +1,10 @@
+unset GOROOT
+unset GOPATH
+
 export GOENV_ROOT=$HOME/.goenv
-export PATH=$GOENV_ROOT/bin:$HOME/go/bin:$PATH
-#export GOENV_DISABLE_GOPATH=1
+export PATH=$GOENV_ROOT/bin:$PATH
 
 eval "$(goenv init -)"
 
-if [ ! -z "$GOROOT" ]; then
-    export PATH="$GOROOT/bin:$PATH";
-fi
-
-if [ ! -z "$GOPATH" ]; then
-    export PATH="$GOPATH/bin:$PATH";
-fi
+export PATH="$GOROOT/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
