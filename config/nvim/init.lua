e1db38f6c9d96870fb5dccbbfd2634c8c518aa3a
Yoko OYAMA <yowcow@gmail.com>
2023-08-23 10:32:58 +0900

    disable

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -138,6 +138,7 @@ require 'nvim-treesitter.configs'.setup {
   sync_install = false,
   highlight = {
     enable = true,
+    disable = {'perl', 'erlang'},
     additional_vim_regex_highlighting = true,
   }
 }
