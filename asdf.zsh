8912fb94282f93e557efe54789bcec107ab74e41
Yoko OYAMA <yowcow@gmail.com>
2024-12-12 15:56:53 +0900

    sync set env

--- asdf.zsh
+++ asdf.zsh
@@ -1,8 +1,10 @@
-for f in $HOME/.asdf/asdf.sh $HOME/.asdf/plugins/golang/set-env.zsh; do
+for f in $HOME/.asdf/asdf.sh $HOME/.asdf/plugins/golang/set-env.bash; do
     if [ -f $f ]; then
         . $f;
     fi
 done
+
+export PATH=$GOBIN:$PATH
 
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
