7f043542ee6d2f12cc367daf47d2c0b527abac4a
Yoko OYAMA <yowcow@gmail.com>
2024-12-12 17:48:30 +0900

    Revert "sync set env"
    This reverts commit 8912fb94282f93e557efe54789bcec107ab74e41.

--- asdf.zsh
+++ asdf.zsh
@@ -1,10 +1,8 @@
-for f in $HOME/.asdf/asdf.sh $HOME/.asdf/plugins/golang/set-env.bash; do
+for f in $HOME/.asdf/asdf.sh $HOME/.asdf/plugins/golang/set-env.zsh; do
     if [ -f $f ]; then
         . $f;
     fi
 done
-
-export PATH=$GOBIN:$PATH
 
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
