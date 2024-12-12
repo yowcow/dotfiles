2cbbfe324ef383e3b853e49799112d8505374dc7
Yoko OYAMA <yowcow@gmail.com>
2024-12-12 15:39:18 +0900

    asdf golang

--- asdf.zsh
+++ asdf.zsh
@@ -1,4 +1,8 @@
-. "$HOME/.asdf/asdf.sh"
+for f in $HOME/.asdf/asdf.sh $HOME/.asdf/plugins/golang/set-env.zsh; do
+    if [ -f $f ]; then
+        . $f;
+    fi
+done
 
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
