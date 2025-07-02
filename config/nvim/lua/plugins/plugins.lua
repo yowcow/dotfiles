6159f5da5ee393d35f6c9530b15eb0cc9ff1c478
Yoko OYAMA <yowcow@gmail.com>
2025-07-02 10:57:01 +0900

    partial

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -101,7 +101,16 @@ return {
       },
     },
   },
-  { "yowcow/partial.nvim", opts = {} },
+  {
+    "yowcow/partial.nvim",
+    opts = {
+      json = { "jq", "." },
+      sql = { "sql-formatter", "--config", vim.fn.expand("~/.config/sql-formatter/config.json") },
+      sqlfmt = { "vacuum", "--", "sqlfmt", "--no-jinjafmt", "--quiet", "--safe", "-" },
+      xml = { "xmllint", "--format", "-" },
+      yaml = { "yamlfmt", "-in" },
+    },
+  },
   { "stevearc/conform.nvim", opts = {
     formatters_by_ft = {
       go = { "goimports" },
