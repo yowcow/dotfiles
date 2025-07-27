7674f62840deb53e88755fd00498c40c80a83938
Yoko OYAMA <yowcow@gmail.com>
2025-07-27 17:00:35 +0900

    tweak

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -23,12 +23,10 @@ return {
   -- change trouble config
   {
     "folke/trouble.nvim",
+    enabled = false,
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
