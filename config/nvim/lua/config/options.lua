196ab28f7e14ba7ff68da9b2cd586009450a153f
Yoko OYAMA <yowcow@gmail.com>
2025-07-30 17:14:45 +0900

    ftplugin

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -8,9 +8,9 @@ vim.opt.laststatus = 3
 vim.opt.listchars = "tab:>-,trail:^"
 vim.opt.mouse = ""
 vim.opt.relativenumber = false
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.expandtab = true
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
 vim.opt.wrap = true
 
 vim.g.snacks_animate = false
