4d99476134315ccec2c51dc7b7b425278764ca26
Yoko OYAMA <yowcow@gmail.com>
2025-09-18 10:02:13 +0900

    nvim-mini/mini.pairs

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -30,7 +30,7 @@ return {
         ft = { "markdown", "Avante" },
       },
       -- --- The below dependencies are optional,
-      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-mini/mini.pick", -- for file_selector provider mini.pick
       -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,5 +1,5 @@
 return {
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "junegunn/fzf" },
   { "junegunn/fzf.vim" },
   {
