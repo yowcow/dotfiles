6bc39c8609e14c4b3551113f5542fa1ddb14c87d
Yoko OYAMA <yowcow@gmail.com>
2026-01-07 10:27:36 +0900

    tweak path

--- config/nvim/lua/plugins/tweaks.lua
+++ config/nvim/lua/plugins/tweaks.lua
@@ -21,4 +21,13 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_c[4] = {
+        "filename",
+        path = 1,
+      }
+    end,
+  },
 }
