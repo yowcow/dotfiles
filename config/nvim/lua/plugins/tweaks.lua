7344c49afe26a444fc1a2c0090be06ae8cdc185f
Yoko OYAMA <yowcow@gmail.com>
2026-01-07 18:16:23 +0900

    lualine tweaks

--- config/nvim/lua/plugins/tweaks.lua
+++ config/nvim/lua/plugins/tweaks.lua
@@ -24,10 +24,8 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      opts.sections.lualine_c[4] = {
-        "filename",
-        path = 1,
-      }
+      opts.sections.lualine_c[4] = { "filename", path = 1 }
+      -- opts.sections.lualine_c[4] = { LazyVim.lualine.pretty_path({ length = 6 }) }
     end,
   },
 }
